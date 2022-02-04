ba53021945e60613f74b4c1dd922bdd542eac3d2
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-04 21:00:43 +0300

    function and constructor created

--- 1_Heritance.sol
+++ 1_Heritance.sol
@@ -4,5 +4,17 @@ pragma solidity >=0.8.7;
 //to their kid, once the kid reaches the desired age.
 
 contract Heritance {
-    
+    address public myKidAccount;
+    uint public releaseDate;
+
+    contructor(address _myKidAccount, uint timeToMaturity) payable {
+        releaseDate = block.timestamp + timeToMaturity;
+        myKidAccount = _myKidAccount;
+    }
+
+    function withdraw() external {
+        require(block.timestamp > maturity, "still there is time")
+        require(msg.sender == kid, "you are not my child");
+        payable(msg.sender).transfer(address(this).balance);
+    }
 }
