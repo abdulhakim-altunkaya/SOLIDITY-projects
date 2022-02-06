4c92569713fe51135e3cb273685a7ef2ba190620
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-06 13:34:06 +0300

    functions improved, repetitive withdrawal prevented, only parents can add kid

--- 2_HeritanceV2.sol
+++ 2_HeritanceV2.sol
@@ -5,8 +5,16 @@ pragma solidity >=0.8.7;
 contract Heritance {
     mapping(address => uint) public amounts;
     mapping(address => uint) public timeLeft;
+    mapping(address => bool) public paid;
+    address public admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
 
     function addKid(address kid, uint timeToMaturity) external payable {
+        require(msg.sender == admin, "only parents can use this function");
+        require(amounts[msg.sender] == 0, "kid can withdraw");
         amounts[kid] = msg.value;
         timeLeft[kid] = block.timestamp + timeToMaturity;
     }
@@ -14,6 +22,8 @@ contract Heritance {
     function withdraw() external {
         require(timeLeft[msg.sender] <= block.timestamp, "you arent mature yet");
         require(amounts[msg.sender] > 0, "only kid can withdraw");
+        require(paid[msg.sender] == false, "dont steal");
+        paid[msg.sender] = true;
         payable(msg.sender).transfer(amounts[msg.sender]);
     }
 }
