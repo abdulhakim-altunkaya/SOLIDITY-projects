37f67a418020840ff8a754f7892fa24711b48ffa
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-05-23 23:19:56 +0300

    voting contract further improved with proposal selection and election closing functions.

--- voting/Voting.sol
+++ voting/Voting.sol
@@ -14,24 +14,61 @@ contract Voting {
     }
 
     address[] internal votedAddresses;
-    string[] public proposals;
+    string[] internal proposals;
+    string public currentProposal;
+    string[] internal passedProposals;
 
-    mapping(string => address[]) internal votedProposals;
-
-    uint public yesCount = 0;
-    uint public noCount = 0;
-    uint public abstentionCount = 0;
-
+    //this part is for future voting count and voting result functions.
+    uint internal yesCount = 0;
+    uint internal noCount = 0;
+    uint internal abstentionCount = 0;
 
 
+    //Currently and account can make a proposal. In future I need to limit this to only stakeholders.
+    function submitProposals(string memory _proposal) public {
+        proposals.push(_proposal);
+    }
+
+    //Only owner can choose which proposal should be put on election. In future, I need to replace owner power
+    //with majority will. 
+    function selectProposals(uint _number) public onlyOwner {
+        currentProposal = proposals[_number];
+        if(_number == proposals.length-1) {
+            proposals.pop();
+        } else {
+            proposals[_number] = proposals[proposals.length-1];
+            proposals.pop();
+        }
+
+    }
+
+    //I need to deepen the code with result functions.
+    function closeVoting() public onlyOwner {
+        passedProposals.push(currentProposal);
+        currentProposal = "";
+    }
+
+    //To see a specific proposal from Waiting proposals, just enter an index number
     function seeProposals(uint _number) public view returns(string memory) {
         return proposals[_number-1];
     }
 
-    function addProposals(string memory _proposal) public onlyOwner {}
-    /*
-    - a time limit on the poposal voting 
-    - require for seePrposals function 
-    */
+    //See  proposal on agenda.
+    function seeMainProposal() public view returns(string memory) {
+        return currentProposal;
+    }
+
+    //See waiting proposals
+    function getProposals() external view returns(string[] memory) {
+        return proposals;
+    }
+
+    //see passed proposals.
+    function getPassedProposals() external view returns(string[] memory) {
+        return passedProposals;
+    }
+
+    //passed proposals = accepted and declined proposals
+    //number limitations for all uint parameter functions.
 
 }
